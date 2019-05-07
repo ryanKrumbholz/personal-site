c2ce88f0ff657e17e766788c7678c0aedb9a22eb
Ryan Krumbholz <ryankrumbholz@10-21-83-230.dynapool.wireless.nyu.edu>
2019-05-06 23:02:24 -0400

    update

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -9,7 +9,7 @@ const experience = props => {
     return (
         <div className="experience-section">
             <ul>
-                <li><ExperienceElem expandableToggle={props.expandableToggleHandler} show={props.show} title="Computer Science Teaching Assistant"
+                <li><ExperienceElem expandableToggle={props.expandableToggleHandler} show={props.show} title="Computer Science TA"
                     company="Seattle Central College - 2018" desc="Assisted in teaching the fundamentals of computer science and OOP. Languages used: Python and Java.
                      Responsibilities: reinforce fundamentals of programming with students that needed assistance, assist students via email, and occasionally
                       lecture about the fundamentals of programming."/>
