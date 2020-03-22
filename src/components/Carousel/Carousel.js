5ab86d201ae1d96a93eb8167a3293deba277cdb3
ryanKrumbholz <krumbholz98@gmail.com>
2020-03-22 08:31:13 -0400

    Added another card and updated about

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -8,6 +8,9 @@ const carousel = props => (
     <Card img={require('../../assets/trump-tracker.png')} title="Tweet Tracking Bot" desc="Developed a Twitter bot that tracks the number of times
     that an individual tweets daily and hourly. For this project the Twitter api, Tweepy api, and Python were used." github_logo={require('../../assets/github_logo.png')} link_logo={require('../../assets/link.png')}
         github_link="https://github.com/ryanKrumbholz/trump-twitter-tracker" link="https://twitter.com/trumptrackernow"/>
+    <Card img={require('../../assets/mathup.png')} title="MathUp!" desc="Currently developing an Android app that allows the user to practice and learn elementary level math.
+     Built with Kotlin, Firebase, and a noSQL database." github_logo={require('../../assets/github_logo.png')} link_logo={require('../../assets/link.png')}
+        github_link="https://github.com/ryanKrumbholz/mathup-app" link="https://github.com/ryanKrumbholz/mathup-app"/>
     </div>
 );
 
