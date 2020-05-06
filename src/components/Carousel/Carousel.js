194d70d5a55ea032ab404dfb8e9fbbde98193473
ryanKrumbholz <krumbholz98@gmail.com>
2020-05-06 06:50:40 -0400

    Updated desc for expensey

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -11,8 +11,8 @@ const carousel = props => (
         <Card img={require('../../assets/mathup.png')} title="MathUp!" desc="Currently developing an Android app that allows the user to practice and learn elementary level math.
         Built with Kotlin, Firebase, and a noSQL database." github_logo={require('../../assets/github_logo.png')} link_logo={require('../../assets/link.png')}
             github_link="https://github.com/ryanKrumbholz/mathup-app" link="https://github.com/ryanKrumbholz/mathup-app"/>
-        <Card img={require('../../assets/expensey.png')} title="Expensey" desc="Currently developing an Expensify clone
-        with the MERN stack." github_logo={require('../../assets/github_logo.png')} link_logo={require('../../assets/link.png')}
+        <Card img={require('../../assets/expensey.png')} title="Expensey" desc="Developed an expense tracking full-stack web application for individuals and small businesses.
+        Built with the MERN stack." github_logo={require('../../assets/github_logo.png')} link_logo={require('../../assets/link.png')}
         github_link="https://github.com/ryanKrumbholz/expensify-clone" link="https://expensey.app"/>
     </div>
 );
