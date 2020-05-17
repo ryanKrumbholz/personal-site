2f4d25880be6c3931256d2ae4b5c6fe7d47aad0f
ryanKrumbholz <krumbholz98@gmail.com>
2020-05-17 08:50:24 -0400

    Fixed mobile sidebar

--- src/components/Sidedrawer/Sidedrawer.js
+++ src/components/Sidedrawer/Sidedrawer.js
@@ -6,14 +6,15 @@ const sideDrawer = props => {
     if (props.show) {
         drawerClasses = ['side-drawer open'];
     }
+    const resume_pdf = require('../../assets/resume.pdf')
     return (
         <div className={drawerClasses}>
             <ul>
-                <li><h2><a href="index.html" onClick={props.click}>Home</a></h2></li>
-                <li><h2><a href="#about" onClick={props.click}>About</a></h2></li>
-                <li><h2><a href="#experience" onClick={props.click}>Experience</a></h2></li>
+                <li><h2><a href="#about" onClick={props.click}>Skills</a></h2></li>
+                <li><h2><a href={resume_pdf} onClick={props.click}>Resume</a></h2></li>
                 <li><h2><a href="#projects" onClick={props.click}>Projects</a></h2></li>
-                <li><h2><a href="#contact" onClick={props.click}>Contact</a></h2></li>
+                <li><h2><a href="#experience" onClick={props.click}>Experience</a></h2></li>
+                <li><h2><a href="mailto:rak535@nyu.edu" target="_blank" onClick={props.click}>Contact</a></h2></li>
             </ul> 
         </div>
     );
